907f7c3965210c54289ad98b7f7c8681bd7841bd
moambk <amir.bekhouche12@gmail.com>
2025-02-03 15:43:46 +0100

    sqd

--- assets/js/main-detail.js
+++ assets/js/main-detail.js
@@ -10,9 +10,9 @@ const serviceData = {
     text_1: 'Uber est un projet Universitaire constitué de 5 membres, c\'est donc un travail d\'équipe qui m\'as permis d\'apprendre a gérer une équipe et développer mon travail en équipe et ma communication.',
     text_2: 'Ce project demandais de recrée le site web Uber, UberEats, UberVélo depuis Laravel controlé par des sprints nous devions donc gerer notre temps et nos missions individuels et misions partagés. Cela concerne le payment du panier de UberEats, la réservation de course Uber ect.. . Une durée de 4 mois nous a été permise, donc 5 semaines de développement. Le reste du temps permettait de conceptualisé le projet à l\'aide de Diagramme de Classe, Diagramme de UseCase etc., une gestion de base de données, grâce à un MDC, MDL et un temps pour gérer la partie des protections des données par rapport au Cookies et autres.',
     img_lien : 'https://github.com/moambk/Uber' ,
-    img_project : '../../assets/img/UberProject.PNG' ,
-    img_project_2 : '../../assets/img/UberEatProject.PNG'  ,
-    img_github : '../../assets/img/Uber-github.PNG' ,
+    img_project : '../../assets/img/UberProject.png' ,
+    img_project_2 : '../../assets/img/UberEatProject.png'  ,
+    img_github : '../../assets/img/Uber-github.png' ,
   },
 
   'last-night': {
@@ -24,9 +24,9 @@ const serviceData = {
     text_1: 'Last Night est un projet de jeu universitaire WPF utilisant donc XAML, C#. Nous devions donc à 3 conceptualisée le code réel et gérer mouvement, collison, micro-IA, pathfinding ect.. .',
     text_2: 'Ce projet demandais de crée un jeu sans réel conditions. Il fallait seulement donnée cour à notre liberté dans un temps restraint de 2 semaine. ',
     img_lien : 'https://github.com/moambk/Last-Night' ,
-    img_project : '../../assets/img/LastNightProject-1.PNG' ,
-    img_project_2 : '../../assets/img/LastNightProject-2.PNG' ,
-    img_github : '../../assets/img/LastNight-github.PNG' ,
+    img_project : '../../assets/img/LastNightProject-1.png' ,
+    img_project_2 : '../../assets/img/LastNightProject-2.png' ,
+    img_github : '../../assets/img/LastNight-github.png' ,
   },
   'labyrinth': {
     title: 'Labyrinth Of Horror',
@@ -37,9 +37,9 @@ const serviceData = {
     text_1: 'Labyrinth Of Horror est un projet universitaire codé grâce au moteur de jeu Unity et des scripts C#, avec une librairie d\'assets pour avoir des personnages beacoup plus réaliste.',
     text_2: 'Ce projet demandais de crée un jeu avec un thème choisi à l\'avance, un thème horreur avait été choisi pour notre groupe. J\'ai été chargé du pathfinding, la gestion de l\'animation du monstre du \'labyrinth\', la gestion de la map a été généré grâce à un scirpt et la gesttion de lumière pour effrayer ont été à ma charge.',
     img_lien : 'https://github.com/moambk/Labyrinth-Of-Horror' ,
-    img_project : '../../assets/img/labyrinthProject.PNG' ,
-    img_project_2 : '../../assets/img/labyrinthProject_2.PNG' ,
-    img_github : '../../assets/img/LabyrinthOfHorror-github.PNG' ,
+    img_project : '../../assets/img/labyrinthProject.png' ,
+    img_project_2 : '../../assets/img/labyrinthProject_2.png' ,
+    img_github : '../../assets/img/LabyrinthOfHorror-github.png' ,
   },
   'dortan': {
     title: 'Dortan',
@@ -50,9 +50,9 @@ const serviceData = {
     text_1: 'Dortan est un pojet universitaire qui nous represente en tant que mairie de Dortan, nous devons donc gérer les missions de la ville comme ramasser les déchets, l\objectif était de crée une application pour visualiser et gérer ses fonctionnalitées.',
     text_2: 'Ce projet d\'appliaction WPF demandé de lié une base de donnée postgres SQL pour permttre de visualiser depuis un compte admin les différentes missions disponible pour permettre de les assigner à des employés ou alors de visualisé celle déjà en cours ou réservé. Codé avec des scipt C# j\'ai donc trvaillé seul pour un travail normalement assigné à 3 personnes, par manque de personnes j\'ai su m\'adapter pour ne pas être pénaliser pour une durée de 3 semaines.',
     img_lien : 'https://github.com/moambk/Dortan' ,
-    img_project : '../../assets/img/DortanProject.PNG' ,
-    img_project_2 : '../../assets/img/DortanProject_2.PNG' ,
-    img_github : '../../assets/img/Dortan-github.PNG' ,
+    img_project : '../../assets/img/DortanProject.png' ,
+    img_project_2 : '../../assets/img/DortanProject_2.png' ,
+    img_github : '../../assets/img/Dortan-github.png' ,
   }
 };
 
