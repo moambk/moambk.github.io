1cf622bfef04a1f55297922d39ce4d78b8efbc24
moambk <amir.bekhouche12@gmail.com>
2025-02-03 14:58:25 +0100

    allé on va dire ca va

--- assets/js/main-detail.js
+++ assets/js/main-detail.js
@@ -1,23 +1,5 @@
 const links = document.querySelectorAll('.services-list a');
 
-// Ajouter un événement de clic à chaque lien
-links.forEach(link => {
-  link.addEventListener('click', function(event) {
-    // Empêcher l'action par défaut (surtout si c'est un lien avec #)
-    event.preventDefault();
-
-    // Retirer la classe "active" de tous les liens
-    links.forEach(l => l.classList.remove('active'));
-
-    // Ajouter la classe "active" au lien cliqué
-    this.classList.add('active');
-  });
-});
-
-
-
-
-
 const serviceData = {
   'uber-projects': {
     title: 'Uber Project',
@@ -28,6 +10,8 @@ const serviceData = {
     text_1: 'Uber est un projet Universitaire constitué de 5 membres, c\'est donc un travail d\'équipe qui m\'as permis d\'apprendre a gérer une équipe et développer mon travail en équipe et ma communication.',
     text_2: 'Ce project demandais de recrée le site web Uber, UberEats, UberVélo depuis Laravel controlé par des sprints nous devions donc gerer notre temps et nos missions individuels et misions partagés. Cela concerne le payment du panier de UberEats, la réservation de course Uber ect.. . Une durée de 4 mois nous a été permise, donc 5 semaines de développement. Le reste du temps permettait de conceptualisé le projet à l\'aide de Diagramme de Classe, Diagramme de UseCase etc., une gestion de base de données, grâce à un MDC, MDL et un temps pour gérer la partie des protections des données par rapport au Cookies et autres.',
     img_lien : 'https://github.com/moambk/Uber' ,
+    img_project : '../../assets/img/UberProject.PNG' ,
+    img_project_2 : '../../assets/img/DortanProject.PNG' ,
     img_github : '../../assets/img/Uber-github.PNG' ,
   },
   'last-night': {
@@ -39,6 +23,8 @@ const serviceData = {
     text_1: 'Last Night est un projet de jeu universitaire WPF utilisant donc XAML, C#. Nous devions donc à 3 conceptualisée le code réel et gérer mouvement, collison, micro-IA, pathfinding ect.. .',
     text_2: 'Ce projet demandais de crée un jeu sans réel conditions. Il fallait seulement donnée cour à notre liberté dans un temps restraint de 2 semaine. ',
     img_lien : 'https://github.com/moambk/Last-Night' ,
+    img_project : '../../assets/img/LastNightProject-1.PNG' ,
+    img_project_2 : '../../assets/img/LastNightProject-2.PNG' ,
     img_github : '../../assets/img/LastNight-github.PNG' ,
   },
   'labyrinth': {
@@ -50,6 +36,8 @@ const serviceData = {
     text_1: 'Labyrinth Of Horror est un projet universitaire codé grâce au moteur de jeu Unity et des scripts C#, avec une librairie d\'assets pour avoir des personnages beacoup plus réaliste.',
     text_2: 'Ce projet demandais de crée un jeu avec un thème choisi à l\'avance, un thème horreur avait été choisi pour notre groupe. J\'ai été chargé du pathfinding, la gestion de l\'animation du monstre du \'labyrinth\', la gestion de la map a été généré grâce à un scirpt et la gesttion de lumière pour effrayer ont été à ma charge.',
     img_lien : 'https://github.com/moambk/Labyrinth-Of-Horror' ,
+    img_project : '../../assets/img/labyrinthProject.PNG' ,
+    img_project_2 : '../../assets/img/labyrinthProject_2.PNG' ,
     img_github : '../../assets/img/LabyrinthOfHorror-github.PNG' ,
   },
   'dortan': {
@@ -59,27 +47,25 @@ const serviceData = {
     check_2: 'Travail individuel',
     check_3: 'WPF, XAML, C#, postgres, SQL',
     text_1: 'Dortan est un pojet universitaire qui nous represente en tant que mairie de Dortan, nous devons donc gérer les missions de la ville comme ramasser les déchets, l\objectif était de crée une application pour visualiser et gérer ses fonctionnalitées.',
-    text_2: 'Ce projet d\'appliaction WPF demandé de lié une base de donnée postgres SQL pour permttre de visualiser depuis un compte admin les différentes missions disponible pour permettre de les assigner à des employés ou alors de visualisé celle déjà en cours ou réservé. Codé avec des scipt C# j\'ai donc trvaillé seul pour un travail normalement assigné à 3 personnes, par manque de personnes j\'ai su m\'adapter pour ne pas être pénaliser pour une durée de 3 semaines ',
+    text_2: 'Ce projet d\'appliaction WPF demandé de lié une base de donnée postgres SQL pour permttre de visualiser depuis un compte admin les différentes missions disponible pour permettre de les assigner à des employés ou alors de visualisé celle déjà en cours ou réservé. Codé avec des scipt C# j\'ai donc trvaillé seul pour un travail normalement assigné à 3 personnes, par manque de personnes j\'ai su m\'adapter pour ne pas être pénaliser pour une durée de 3 semaines.',
     img_lien : 'https://github.com/moambk/Dortan' ,
+    img_project : '../../assets/img/DortanProject.PNG' ,
+    img_project_2 : '../../assets/img/DortanProject_2.PNG' ,
     img_github : '../../assets/img/Dortan-github.PNG' ,
   }
 };
 
-// Fonction pour changer le texte et l'élément actif
+
 links.forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault();
 
-    // Retirer la classe "active" de tous les liens
     links.forEach(l => l.classList.remove('active'));
 
-    // Ajouter la classe "active" au lien cliqué
     this.classList.add('active');
 
-    // Récupérer l'ID du service à partir de l'attribut data-service
     const serviceId = this.getAttribute('data-service');
 
-    // Modifier le titre et la description en fonction du service sélectionné
     document.getElementById('service-title').textContent = serviceData[serviceId].title;
     document.getElementById('service-description').textContent = serviceData[serviceId].description;
     document.getElementById('check_1').textContent = serviceData[serviceId].check_1;
@@ -89,5 +75,9 @@ links.forEach(link => {
     document.getElementById('text_2').textContent = serviceData[serviceId].text_2;
     document.getElementById('img_lien').setAttribute('href', serviceData[serviceId].img_lien);
     document.getElementById('img_github').setAttribute('src', serviceData[serviceId].img_github);
+    document.getElementById('img_project').setAttribute('src', serviceData[serviceId].img_project);
+    document.getElementById('img_project_2').setAttribute('src', serviceData[serviceId].img_project_2);
+
+    
   });
 });
